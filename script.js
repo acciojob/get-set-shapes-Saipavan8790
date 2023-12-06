a5cdee60cc4c9bc5b9bcbc23a44b1f67e61ecb60
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-12-07 00:09:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//complete this code
 class Rectangle {
-	class Rectangle {
   constructor(width, height) {
     this.width = width;
     this.height = height;
@@ -20,13 +18,13 @@ class Rectangle {
 }
 
 class Square extends Rectangle {
-	constructor(side) {
+  constructor(side) {
     // In a square, width and height are the same
     super(side, side);
   }
 
   getPerimeter() {
-    return 4 * this.width; // Perimeter of a square: 4 * side
+    return 4 * this.getWidth(); // Perimeter of a square is 4 times the side
   }
 }
 
