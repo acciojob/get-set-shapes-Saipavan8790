552976ea7c3cc45af8174f30b8531d0b967fd934
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-12-07 00:04:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ class Rectangle {
   }
 }
 
-class Square extends Animal {
+class Square extends Rectangle {
 	constructor(side) {
     // In a square, width and height are the same
     super(side, side);
